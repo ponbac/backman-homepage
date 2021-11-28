6d025a6687a991128e0a04c94a7a390fbd383641
Pontus Backman <ponbac@student.chalmers.se>
2021-11-29 00:33:39 +0100

    began work on animation

--- pages/index.js
+++ pages/index.js
@@ -9,113 +9,116 @@ import {
   useColorModeValue
 } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
+import Layout from '../components/layouts/article';
 import Section from '../components/section';
 import Paragraph from '../components/paragraph';
 import { BioSection, BioYear } from '../components/bio';
 
 const Page = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;m a full-stack developer based in Sweden!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Pontus Backman
-          </Heading>
-          <p>Digital Craftsman (Developer / Designer)</p>
-        </Box>
+    <Layout title="Home">
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/pontus.jpg"
-            alt="Profile Image"
-          />
+          Hello, I&apos;m a full-stack developer based in Sweden!
         </Box>
-      </Box>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          imperdiet semper sodales. Sed non risus turpis. Integer venenatis
-          sollicitudin nisl, sit amet accumsan quam eleifend vel. Aenean at
-          luctus mi. Nunc sem massa, dictum eget sem sit amet, fringilla laoreet
-          nisl. Vestibulum lectus ex,{' '}
-          <NextLink href="/works">
-            <Link>efficitur</Link>
-          </NextLink>{' '}
-          id condimentum et, semper ac risus. Aliquam a elementum tellus. Cras
-          id fermentum nunc, vel porttitor sapien. Nullam in ornare dolor, vitae
-          aliquam augue. Aliquam consectetur pellentesque efficitur. Sed
-          facilisis ac diam non consequat. Vestibulum at velit lorem.
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Pontus Backman
+            </Heading>
+            <p>King of the Interwebs (Gigahacker/Developer)</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/pontus.jpg"
+              alt="Profile Image"
+            />
+          </Box>
         </Box>
-      </Section>
 
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1998</BioYear>
-          Born in Sundsvall, Sweden.
-        </BioSection>
-        <BioSection>
-          <BioYear>2017</BioYear>
-          Began studies in Software Engineering at Chalmers University of
-          Technology
-        </BioSection>
-        <BioSection>
-          <BioYear>2020</BioYear>
-          Finished my Bachelor&apos;s in Software Engineering
-        </BioSection>
-        <BioSection>
-          <BioYear>2022</BioYear>
-          Finished my Master&apos;s in Software Engineering, thesis on TODO
-        </BioSection>
-      </Section>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            imperdiet semper sodales. Sed non risus turpis. Integer venenatis
+            sollicitudin nisl, sit amet accumsan quam eleifend vel. Aenean at
+            luctus mi. Nunc sem massa, dictum eget sem sit amet, fringilla
+            laoreet nisl. Vestibulum lectus ex,{' '}
+            <NextLink href="/works">
+              <Link>efficitur</Link>
+            </NextLink>{' '}
+            id condimentum et, semper ac risus. Aliquam a elementum tellus. Cras
+            id fermentum nunc, vel porttitor sapien. Nullam in ornare dolor,
+            vitae aliquam augue. Aliquam consectetur pellentesque efficitur. Sed
+            facilisis ac diam non consequat. Vestibulum at velit lorem.
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
 
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          Interested in
-        </Heading>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-          imperdiet semper sodales. Sed non risus turpis. Integer venenatis
-          sollicitudin nisl, sit amet accumsan quam eleifend vel. Aenean at
-          luctus mi. Nunc sem massa, dictum eget sem sit amet, fringilla laoreet
-          nisl. Vestibulum lectus ex, efficitur id condimentum et, semper ac
-          risus. Aliquam a elementum tellus.
-        </Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1998</BioYear>
+            Born in Sundsvall, Sweden.
+          </BioSection>
+          <BioSection>
+            <BioYear>2017</BioYear>
+            Began studies in Software Engineering at Chalmers University of
+            Technology
+          </BioSection>
+          <BioSection>
+            <BioYear>2020</BioYear>
+            Finished my Bachelor&apos;s in Software Engineering
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Finished my Master&apos;s in Software Engineering, thesis on TODO
+          </BioSection>
+        </Section>
+
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            Interested in
+          </Heading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            imperdiet semper sodales. Sed non risus turpis. Integer venenatis
+            sollicitudin nisl, sit amet accumsan quam eleifend vel. Aenean at
+            luctus mi. Nunc sem massa, dictum eget sem sit amet, fringilla
+            laoreet nisl. Vestibulum lectus ex, efficitur id condimentum et,
+            semper ac risus. Aliquam a elementum tellus.
+          </Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   );
 };
 
--- pages/works.js
+++ pages/works.js
@@ -1,4 +1,5 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react';
+import Layout from '../components/layouts/article';
 import Section from '../components/section';
 import { WorkGridItem } from '../components/grid-item';
 
@@ -8,42 +9,48 @@ import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch
 import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png';
 
 const Works = () => (
-  <Container>
-    <Heading as="h3" fontSize={20} mb={4}>
-      Works
-    </Heading>
+  <Layout title="Works">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
 
-    <SimpleGrid columns={[1, 1, 2]} gap={6}>
-      <Section>
-        <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
-          A Markdown note-taking app with 100+ plugins, cross-platform and
-          encrypted data sync support
-        </WorkGridItem>
-      </Section>
-      <Section>
-        <WorkGridItem id="walknote" title="walknote" thumbnail={thumbWalknote}>
-          Music recommendation app for iOS
-        </WorkGridItem>
-      </Section>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
+            A Markdown note-taking app with 100+ plugins, cross-platform and
+            encrypted data sync support
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            id="walknote"
+            title="walknote"
+            thumbnail={thumbWalknote}
+          >
+            Music recommendation app for iOS
+          </WorkGridItem>
+        </Section>
 
-      <Section delay={0.1}>
-        <WorkGridItem
-          id="fourpainters"
-          title="The four painters"
-          thumbnail={thumbFourPainters}
-        >
-          A video work generated with deep learning, imitating famous four
-          painters like Van Gogh
-        </WorkGridItem>
-      </Section>
-      <Section delay={0.1}>
-        <WorkGridItem id="menkiki" thumbnail={thumbMenkiki} title="Menkiki">
-          An app that suggests ramen(noodle) shops based on a given photo of the
-          ramen you want to eat
-        </WorkGridItem>
-      </Section>
-    </SimpleGrid>
-  </Container>
+        <Section delay={0.1}>
+          <WorkGridItem
+            id="fourpainters"
+            title="The four painters"
+            thumbnail={thumbFourPainters}
+          >
+            A video work generated with deep learning, imitating famous four
+            painters like Van Gogh
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.1}>
+          <WorkGridItem id="menkiki" thumbnail={thumbMenkiki} title="Menkiki">
+            An app that suggests ramen(noodle) shops based on a given photo of
+            the ramen you want to eat
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+  </Layout>
 );
 
 export default Works;
