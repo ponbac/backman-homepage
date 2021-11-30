af900461ca206e53f9e107ff4c8d4a156c230860
Pontus Backman <ponbac@student.chalmers.se>
2021-11-30 22:49:19 +0100

    ton of work

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import Navbar from '../navbar'
+import Navbar from '../navbar';
+import NoSsr from '../no-ssr';
 import { Box, Container } from '@chakra-ui/react';
 import VoxelDog from '../voxel-dog';
 
@@ -14,7 +15,9 @@ const Main = ({ children, router }) => {
       <Navbar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        <VoxelDog />
+        <NoSsr>
+          <VoxelDog />
+        </NoSsr>
         {children}
       </Container>
     </Box>
